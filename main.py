7b9cfe0558c8e9446805f18aede81c96213a885b
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-10-19 17:12:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -65,8 +65,7 @@ elif selected=="Dự đoán":
 
     add_selectbox=st.sidebar.selectbox(
     "What Algorithm do you want to use?",
-    ("Linear regression", "Logistic regression", "ARIMA","GRU","LSTM", "RNN","SSA")
-)
+    ("Linear regression", "Logistic regression", "ARIMA","GRU","LSTM", "RNN","SSA"))
 
     df = listing_companies()
     stock_name = st.text_input("Nhập tên cổ phiếu:")
