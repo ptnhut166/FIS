16e93e1f97f672b637db557f3c54a0f67b445aca
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-10-19 16:16:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ with st.sidebar:
     selected = option_menu("Lựa chọn", ["Thông tin", 'Dự đoán','Bot'], 
         icons=['info-square-fill', 'bar-chart-line-fill','reddit'], menu_icon="cast", default_index=1)
     selected
+#_____________________________________________________________________________________  
 
-if selected=="Dự đoán":
-    st.title("Thông tin cổ phiếu")
-elif selected=="Bot":
-    st.title("bot")
-else:
+global stock_name
+
+if selected =="Thông tin":
     st.title("Thông tin")
     df = listing_companies()
+    
     
     stock_name = st.text_input("Nhập tên cổ phiếu:")
     stock_name = stock_name.upper()
@@ -61,7 +61,42 @@ else:
     df_off=company_officers (symbol=stock_name, page_size=20, page=0).dropna()
     df_off=df_off.drop(["ticker"], axis=1)
     st.table(df_off)
-    
+#_____________________________________________________________________________________  
+elif selected=="Dự đoán":
+    st.title("Thông tin cổ phiếu")
 
+    selected_stock=stock_name
+    def main():
+        st.subheader("""Daily **closing price** for """ + selected_stock)
+    #get data on searched ticker
+        stock_data = yf.Ticker(selected_stock)
+    #get historical data for searched ticker
+        stock_df = stock_data.history(period='1d', start='2020-01-01', end=None)
+    #print line chart with daily closing prices for searched ticker
+        st.line_chart(stock_df.Close)
+
+        st.subheader("""Last **closing price** for """ + selected_stock)
+    #define variable today 
+        today = datetime.today().strftime('%Y-%m-%d')
+    #get current date data for searched ticker
+        stock_lastprice = stock_data.history(period='1d', start=today, end=today)
+    #get current date closing price for searched ticker
+        last_price = (stock_lastprice.Close)
+    #if market is closed on current date print that there is no data available
+        if last_price.empty == True:
+            st.write("No data available at the moment")
+        else:
+            st.write(last_price)
+    
+    #get daily volume for searched ticker
+        st.subheader("""Daily **volume** for """ + selected_stock)
+        st.line_chart(stock_df.Volume) 
+
+    
+if __name__ == "__main__":
+    main()
+
+elif selected=="Bot":
+    st.title("bot")
 
 
