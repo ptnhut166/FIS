31fcdda3b655dbbdbacf709a01a04fcc5f717947
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-10-19 16:31:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ elif selected=="Dự đoán":
         st.header("Thông tin tổng quan")
 
         df = stock_historical_data(stock_name, "2022-01-01", "2023-10-10", "1D", "stock")
-        fig = candlestick_chart(df, ma_periods=[50,200], show_volume=False, reference_period=300, figure_size=(15, 8), 
+        fig = candlestick_chart(df, ma_periods=[50,200], show_volume=False, reference_period=300, figure_size=(10, 6), 
                         title=stock_name+' - Candlestick Chart with MA and Volume', x_label='Date', y_label='Price', 
                         colors=('lightgray', 'gray'), reference_colors=('black', 'blue'))
 
